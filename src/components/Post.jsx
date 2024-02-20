b2907fef95231d41a2c172c25f84ccc8c5cc497e
Thiago Amim <thiagoamim11@gmail.com>
2024-02-20 15:58:47 -0300

    melhorando o Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -19,11 +19,15 @@ export function Post() {
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
 
-                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-                    <p>👉<a href=""> jane.design/doctorcare </a></p>
+                <p>👉<a href=""> jane.design/doctorcare </a></p>
 
-                    <p><a href="">novoprojeto #nlw #rocketseat</a></p>
+                <p>
+                    <a href="">novoprojet</a>{' '}
+                    <a href=''> #nlw</a>{' '}
+                    <a href=''>#rocketseat</a>{' '}
+                </p>
             </div>
         </article>
     )
