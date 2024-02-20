ede1e33d8718895ff3315f3f7a6f6b3bce00518c
Thiago Amim <thiagoamim11@gmail.com>
2024-02-20 14:57:02 -0300

    Seção de avatar pronta

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Post } from "./components/Post"
+import { Header } from "./components/Header"
+import './global.css'
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar/>
+        </aside>
+
+        <main>
+          <Post
+            author="Thiago Amim"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint ut nulla consectetur explicabo necessitatibus expedita dolorem? Inventore libero magnam porro molestiae doloremque, sint possimus voluptatem dolores aspernatur voluptates sunt fuga."
+          />
+
+          <Post
+            author="Isela"
+            content="Um novo post da Isela"
+          />
+        </main>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
 export default App
+
+
